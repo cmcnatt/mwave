d717cc7d58799faa0316dfd8b8e5a19618f3f44c
camalamadingdong <cameron.mcnatt@gmail.com>
2015-02-18 13:47:05 +0000

    Remove Examples

--- Examples/BemRuns/nem_1b_6dof_1/nem_run.bat
+++
@@ -1,3 +0,0 @@
-N:\Nemoh\Nemoh_mer\my-nemoh\Release\preProcessor.exe
-N:\Nemoh\Nemoh_mer\my-nemoh\Release\Solver.exe
-N:\Nemoh\Nemoh_mer\my-nemoh\Release\postProcessor.exe
--- Examples/BemRuns/nem_hb_2/nem_run.bat
+++
@@ -1,4 +0,0 @@
-N:\Nemoh\Nemoh_mer\my-nemoh\Release\Mesh.exe >Mesh\Mesh.log
-N:\Nemoh\Nemoh_mer\my-nemoh\Release\preProcessor.exe
-N:\Nemoh\Nemoh_mer\my-nemoh\Release\Solver.exe
-N:\Nemoh\Nemoh_mer\my-nemoh\Release\postProcessor.exe
