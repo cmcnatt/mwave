c7106819c113c2da36211fde1105f9d36cc1f724
gabscarlett <gabriel.scarlett@moceanenergy.com>
2019-09-06 00:45:47 +0100

    no message

---
+++ TEST.m
@@ -0,0 +1,6 @@
+%addpath(genpath('C:\Users\camer\Dropbox\matlab'),'-frozen')
+addpath(genpath('C:\Users\camer\Dropbox\matlab'))
+
+rmpath(genpath('C:\Users\camer\Dropbox\matlab\studies\Controls\Stage_1'))
+
+disp('Paths refreshed');
