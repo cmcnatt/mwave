b4d10a1ea962e502796bc380a3de135148ab36c5
Linus Kemme <linus.kemme@mocean.energy>
2025-05-12 17:03:08 +0100

    autoformatting

--- Body/Geometry/PanelGeo.m
+++ Body/Geometry/PanelGeo.m
@@ -1,11 +1,11 @@
 classdef PanelGeo < handle
-    
+
     properties (Access = private)
         panels;
         xsym;
         ysym;
     end
-    
+
     properties (Dependent)
         Panels;
         Count;
@@ -18,16 +18,16 @@ classdef PanelGeo < handle
         IsBodies;
         AvgPanelArea;
         Xsymmetry;
-        Ysymmetry; 
+        Ysymmetry;
     end
-    
+
     methods
         % Constructor
         function [geo] = PanelGeo(pans, varargin)
             if (nargin >= 1)
                 if (isa(pans, 'PanelGeo'))
                     nPan = pans.Count;
-                    
+
                     for n = 1:nPan
                         newPans(n) = Panel(pans.Panels(n).Vertices);
                         newPans(n).Normal = pans.Panels(n).Normal;
@@ -36,7 +36,7 @@ classdef PanelGeo < handle
                         newPans(n).IsInterior = pans.Panels(n).IsInterior;
                         newPans(n).IsBody = pans.Panels(n).IsBody;
                     end
-                    
+
                     geo.panels = newPans;
                     geo.xsym = pans.Xsymmetry;
                     geo.ysym = pans.Ysymmetry;
@@ -60,73 +60,73 @@ classdef PanelGeo < handle
                 end
             end
         end
-        
+
         function [pans] = get.Panels(geo)
             pans = geo.panels;
         end
-        
+
         function [cnt] = get.Count(geo)
             cnt = length(geo.panels);
         end
-        
+
         function [cents] = get.Centroids(geo)
             cnt = geo.Count;
             cents = zeros(cnt, 3);
-            
+
             for n = 1:cnt
                 cents(n,:) = geo.panels(n).Centroid;
             end
         end
-        
+
         function [norms] = get.Normals(geo)
             cnt = geo.Count;
             norms = zeros(cnt, 3);
-            
+
             for n = 1:cnt
                 norms(n,:) = geo.panels(n).Normal;
             end
         end
-        
+
         function [ars] = get.Areas(geo)
             cnt = geo.Count;
             ars = zeros(cnt, 1);
-            
+
             for n = 1:cnt
                 ars(n) = geo.panels(n).Area;
             end
         end
-        
+
         function [iws] = get.IsWets(geo)
             cnt = geo.Count;
             iws = zeros(cnt, 1);
-            
+
             for n = 1:cnt
                 iws(n) = geo.panels(n).IsWet;
             end
         end
-        
+
         function [iis] = get.IsInteriors(geo)
             cnt = geo.Count;
             iis = zeros(cnt, 1);
-            
+
             for n = 1:cnt
                 iis(n) = geo.panels(n).IsInterior;
             end
         end
-        
+
         function [ibs] = get.IsBodies(geo)
             cnt = geo.Count;
             ibs = zeros(cnt, 1);
-            
+
             for n = 1:cnt
                 ibs(n) = geo.panels(n).IsBody;
             end
         end
-        
+
         function [vals] = get.Values(geo)
             cnt = geo.Count;
             vals = zeros(cnt, 1);
-            
+
             for n = 1:cnt
                 valn = geo.panels(n).Value;
                 if (isempty(valn))
@@ -141,12 +141,12 @@ classdef PanelGeo < handle
             if (length(vals) ~= cnt)
                 error('The number of values must be equal to the number of panels');
             end
-            
+
             for n = 1:cnt
                 geo.panels(n).Value = vals(n);
             end
         end
-        
+
         function [area] = get.AvgPanelArea(geo)
             N = length(geo.panels);
             area = 0;
@@ -155,19 +155,19 @@ classdef PanelGeo < handle
             end
             area = area/N;
         end
-        
+
         function [xsy] = get.Xsymmetry(geo)
             xsy = geo.xsym;
         end
-        
+
         function [ysy] = get.Ysymmetry(geo)
             ysy = geo.ysym;
         end
-        
+
         function [] = set.Ysymmetry(geo,ysy)
             geo.ysym = ysy;
         end
-        
+
         function [newGeo] = GetSubsetPanels(geo, incWet, incInt)
             pans = geo.panels;
             N = length(pans);
@@ -175,29 +175,29 @@ classdef PanelGeo < handle
             if incWet
                 inds = geo.IsWets == true;
             end
-            if ~incInt 
+            if ~incInt
                 inds = inds & (geo.IsInteriors == false);
             end
-            
+
             newGeo = PanelGeo(pans(inds));
         end
-        
+
         function [] = Translate(geo, vector)
             N = length(geo.panels);
             for n = 1:N
                 geo.panels(n).Translate(vector);
             end
         end
-        
+
         function [cents] = OffsetCentroids(geo, dist)
             cnt = geo.Count;
             cents = zeros(cnt, 3);
-            
+
             for n = 1:cnt
                 cents(n, :) = geo.panels(n).OffsetCentroid(dist);
             end
         end
-        
+
         function [] = Rotate(geo, ax, angle, varargin)
             N = length(geo.panels);
             rotMat = createRotationMatrix(ax, angle);
@@ -205,20 +205,20 @@ classdef PanelGeo < handle
                 geo.panels(n).Rotate('RotMat', rotMat, varargin{:});
             end
         end
-                
+
         function [geo1, geo2] = Split(geo, planePnt, planeNorm)
             pt = planePnt;
             nrm = planeNorm;
             nrm = nrm./(nrm(1)^2 + nrm(2)^2 + nrm(3)^2);
-            
+
             a = nrm(1);
             b = nrm(2);
             c = nrm(3);
-            
+
             d = -dot(pt, nrm);
-            
+
             N = length(geo.panels);
-            
+
             nPos = 1;
             nNeg = 1;
             for m = 1:N
@@ -228,7 +228,7 @@ classdef PanelGeo < handle
                 for n = 1:4
                     v = verts(n,:);
                     D = a*v(1) + b*v(2) + c*v(3) + d;
-                    
+
                     if (D < 0)
                         allPos = false;
                     end
@@ -247,11 +247,11 @@ classdef PanelGeo < handle
                     nNeg = nNeg + 1;
                 end
             end
-            
+
             geo1 = PanelGeo(pansNeg);
             geo2 = PanelGeo(pansPos);
         end
-        
+
         function [stl] = MakeStl(geo, cg, noInt)
             if nargin < 2
                 cg = [0 0 0];
@@ -269,7 +269,7 @@ classdef PanelGeo < handle
             end
             verts0 = zeros(4*N, 3);
             faces0 = zeros(2*N, 3);
-            
+
             im = 1;
             ifa = 1;
             for m = 1:N0
@@ -283,26 +283,26 @@ classdef PanelGeo < handle
                 end
             end
             [verts, ~, ic] = unique(verts0, 'rows');
-            
+
             faces = zeros(2*N, 3);
             for m = 1:2*N
                 for n = 1:3
                     faces(m, n) = ic(faces0(m, n));
                 end
             end
-                       
+
             stl = Stl6DOFGeo;
-            
+
             stl.SetStl(verts, faces, cg);
         end
-        
+
         function [] = WriteBpi(geo, fileLoc, name)
             fileName = [fileLoc '\' name '.bpi'];
             fileID = fopen(fileName, 'wt');
-            
+
             ulen = 1;
             g = 9.806650;
-            
+
             pans = geo.Panels;
             nPan = length(pans);
             fprintf(fileID, ['Model ' name ', created: ' date '\n']);
@@ -310,28 +310,28 @@ classdef PanelGeo < handle
             for n = 1:nPan
                 fprintf(fileID, '\t%8.4f\t%8.4f\t%8.4f\n', verts(m,1), verts(m,2), verts(m,3));
             end
-            
+
             fclose(fileID);
         end
-        
+
         function [] = WriteGdf(geo, fileLoc, name, center, includeInt)
             if nargin < 4
                 center = [];
                 includeInt = true;
             end
-            
+
             % make a copy
             geo0 = PanelGeo(geo);
             if ~isempty(center)
                 geo0.Translate(-center); %translate to rotate about center of rotation.
             end
-            
+
             filename = [fileLoc '\' name '.gdf'];
             fid = fopen(filename, 'wt');
-            
+
             ulen = 1;
             g = 9.806650;
-            
+
             Nwet = sum(geo0.IsWets);
             Nint = sum(geo0.IsInteriors);
             if (includeInt)
@@ -339,17 +339,17 @@ classdef PanelGeo < handle
             else
                 count = Nwet - Nint;
             end
-            
+
             fprintf(fid, ['Model ' name ', created: ' date '\n']);
             fprintf(fid, '%8.4f %8.4f\n', ulen, g);
             fprintf(fid, '%i %i \n', geo0.Xsymmetry, geo0.Ysymmetry);
             fprintf(fid, '%i\n', count);
-            
+
             pans = geo0.Panels;
-            
+
             for n = 1:geo0.Count
                 pan = pans(n);
-                
+
                 panOk = true;
                 if (~pan.IsWet)
                     panOk = false;
@@ -357,7 +357,7 @@ classdef PanelGeo < handle
                 if (pan.IsInterior && ~includeInt)
                     panOk = false;
                 end
-                
+
                 if (panOk)
                     verts = pan.Vertices;
                     for m = 1:4
@@ -365,10 +365,10 @@ classdef PanelGeo < handle
                     end
                 end
             end
-            
+
             fclose(fid);
         end
-        
+
         function [faces, verts] = QuadMesh(geo, noInt, onlyWet)
             if nargin < 2
                 noInt = true;
@@ -381,7 +381,7 @@ classdef PanelGeo < handle
             N = sum(inclPan);
             verts0 = zeros(4*N, 3);
             faces0 = zeros(N, 4);
-            
+
             im = 1;
             ifa = 1;
             for m = 1:N0
@@ -394,7 +394,7 @@ classdef PanelGeo < handle
                 end
             end
             [verts, ~, ic] = unique(verts0, 'rows');
-            
+
             faces = zeros(N, 4);
             for m = 1:N
                 for n = 1:4
@@ -402,35 +402,35 @@ classdef PanelGeo < handle
                 end
             end
         end
-                
+
         function [] = plot(geo, varargin)
             geo.plotFuncs('plot', varargin{:});
         end
-        
+
         function [] = surf(geo, varargin)
             geo.plotFuncs('surf', varargin{:});
         end
-        
+
         function [geoOut] = plus(geoa, geob)
             % PLUS: overloade addition operator
-            
+
             if (xor(geoa.Xsymmetry, geob.Xsymmetry) || xor(geoa.Ysymmetry, geob.Ysymmetry))
                 error('PanelGeos must have the same symmetry to add');
             end
-            
+
             nA = geoa.Count;
             nB = geob.Count;
             for n = 1:nA
                 newPans(n) = Panel(geoa.Panels(n).Vertices);
             end
-            
+
             for n = 1:nB
                 newPans(n+nA) = Panel(geob.Panels(n).Vertices);
             end
-            
+
             geoOut = PanelGeo(newPans);
         end
-        
+
         function [geoOut] = times(arg1, arg2)
             % TIMES: overloaded times operator
             if isa(arg1, 'PanelGeo')
@@ -440,12 +440,12 @@ classdef PanelGeo < handle
                 geoIn = arg2;
                 val = arg1;
             end
-            
+
             geoOut = PanelGeo(geoIn);
-            
+
             geoOut.Values = geoIn.Values.*val;
         end
-        
+
         function [geoOut] = mtimes(arg1, arg2)
             % MTIMES: overloaded mtimes operator
             if isa(arg1, 'PanelGeo')
@@ -455,12 +455,12 @@ classdef PanelGeo < handle
                 geoIn = arg2;
                 val = arg1;
             end
-            
+
             geoOut = PanelGeo(geoIn);
-            
+
             geoOut.Values = geoIn.Values*val;
         end
-        
+
         function [geoOut] = rdivide(arg1, arg2)
             % RDIVIDE: overloaded rdivide operator
             if isa(arg1, 'PanelGeo')
@@ -470,39 +470,39 @@ classdef PanelGeo < handle
                 geoIn = arg2;
                 val = arg1;
             end
-            
+
             geoOut = geoIn.*(1./val);
         end
-        
+
         % Overloaded real operator
         function [geoOut] = real(geoIn)
             geoOut = PanelGeo(geoIn);
-            
+
             geoOut.Values = real(geoIn.Values);
         end
-        
+
         % Overloaded imag operator
         function [geoOut] = imag(geoIn)
             geoOut = PanelGeo(geoIn);
-            
+
             geoOut.Values = imag(geoIn.Values);
         end
-        
+
         % Overloaded abs operator
         function [geoOut] = abs(geoIn)
             geoOut = PanelGeo(geoIn);
-            
+
             geoOut.Values = abs(geoIn.Values);
         end
-        
+
         % Overloaded angle operator
         function [geoOut] = angle(geoIn)
             geoOut = PanelGeo(geoIn);
-            
+
             geoOut.Values = anlge(geoIn.Values);
         end
     end
-    
+
     methods (Access = private)
         function inclPan = getIndices(obj, noInt, onlyWet)
             inclPan = true(obj.Count, 1);
@@ -513,7 +513,7 @@ classdef PanelGeo < handle
                 inclPan = inclPan & obj.IsWets;
             end
         end
-        
+
         function plotFuncs(obj, func, varargin)
 
             [opts, args] = checkOptions({{'ShowNorm'}, {'OnlyWet'}, {'NoInt'}, {'color', 1}, {'alpha', 1}, {'edge', 1}, {'line', 1}, {'water'}}, varargin);
@@ -527,7 +527,7 @@ classdef PanelGeo < handle
             alpha = 1;
             if opts(5)
                 alpha = args{5};
-            end           
+            end
             edge = [];
             if opts(6)
                 edge = args{6};
@@ -537,25 +537,25 @@ classdef PanelGeo < handle
                 line = args{7};
             end
             showWater = opts(8);
-            
-            
+
+
             xsy = false;
             ysy = false;
-            
+
             [mesh, verts] = obj.QuadMesh(noInt, onlyWet);
-            
+
             vals = obj.Values;
-            
+
             args = {'faces', mesh, 'vertices', verts,...
-                    'facelighting', 'none', 'facecolor', 'flat', 'edgelighting', 'flat',...
-                    'parent',gca};
-            
+                'facelighting', 'none', 'facecolor', 'flat', 'edgelighting', 'flat',...
+                'parent',gca};
+
             if strcmp(func, 'plot')
                 colV = MColor.Blue;
                 lineW = 0.5;
                 if ~isempty(edge)
-                colV = edge;
-                end 
+                    colV = edge;
+                end
                 if ~isempty(line)
                     lineW = line;
                 end
@@ -571,7 +571,7 @@ classdef PanelGeo < handle
                 args{length(args) + 1} = colV;
                 args{length(args) + 1} = 'facealpha';
                 args{length(args) + 1} = alpha;
-                
+
                 if ~isempty(color)
                     args{length(args) + 1} = 'facecolor';
                     args{length(args) + 1} = color;
@@ -587,13 +587,13 @@ classdef PanelGeo < handle
                     args{length(args) + 1} = 'cdata';
                     args{length(args) + 1} = colF;
                 end
-                
+
                 if ~isempty(edge)
                     args{length(args) + 1} = 'edgecolor';
                     args{length(args) + 1} = edge;
-                end                
+                end
             end
-            
+
             patch(args{:});
 
             if (showNorm)
@@ -603,10 +603,10 @@ classdef PanelGeo < handle
                 norm = obj.Normals;
                 area = obj.Areas;
                 quiver3(cent(ipan,1), cent(ipan,2), cent(ipan,3), norm(ipan,1), norm(ipan,2), norm(ipan,3),'color', MColor.Black);
-%                 h1 = quiver3(cent(ipan,1), cent(ipan,2), cent(ipan,3), norm(ipan,1), norm(ipan,2), norm(ipan,3),'color', MColor.Black,'linewidth',1.5);
-%                 set(h1,'AutoScale','on', 'AutoScaleFactor', 1.5)
+                %                 h1 = quiver3(cent(ipan,1), cent(ipan,2), cent(ipan,3), norm(ipan,1), norm(ipan,2), norm(ipan,3),'color', MColor.Black,'linewidth',1.5);
+                %                 set(h1,'AutoScale','on', 'AutoScaleFactor', 1.5)
             end
-    
+
             if showWater
                 % Find maximum extents of geometry
                 xExtent(1) = min(obj.Centroids(:,1));
@@ -615,12 +615,12 @@ classdef PanelGeo < handle
                 yExtent(2) = max(obj.Centroids(:,2));
 
                 del = 0.4*max([-xExtent(1) xExtent(2) -yExtent(1) yExtent(2)]);
-                        
+
                 water = PanelGeo(...
                     Panel(...
-                    [xExtent(1)-del yExtent(1)-del 0; 
-                    xExtent(2)+del yExtent(1)-del 0; 
-                    xExtent(2)+del yExtent(2)+del 0; 
+                    [xExtent(1)-del yExtent(1)-del 0;
+                    xExtent(2)+del yExtent(1)-del 0;
+                    xExtent(2)+del yExtent(2)+del 0;
                     xExtent(1)-del yExtent(2)+del 0]));
                 [meshWater, vertsWater] = water.QuadMesh(noInt, onlyWet);
                 argsWater = args; % Reuse args from WEC mesh, but change some settings
@@ -630,7 +630,7 @@ classdef PanelGeo < handle
                 argsWater{16} = 0.5; % Add transparency
                 argsWater{18} = MColor.Blue; % Set colour
                 patch(argsWater{:});
-%                 surf(water, 'color', MColor.Blue, 'alpha', 0.5);
+                %                 surf(water, 'color', MColor.Blue, 'alpha', 0.5);
             end
 
         end
